3d531155a3b0e9f881cd13e70a5713f6b5526607
MONTESA LOPEZ, SERGIO <montesa@unizar.es>
2023-01-04 13:00:27 +0100

    feat: mostramos los equipos

--- scraping/index.js
+++ scraping/index.js
@@ -1,4 +1,26 @@
-const res = await fetch('https://kingsleague.pro/estadisticas/clasificacion/')
-const text = await res.text()
+import * as cheerio from 'cheerio'
 
-console.log(text)
+const URL = 'https://kingsleague.pro/estadisticas/clasificacion/'
+
+// async function scrape(url) {
+const scrape = async (url) => {
+  const res = await fetch(url)
+  const html = await res.text()
+  return cheerio.load(html)
+}
+
+const cleanText = text => text
+  .replace(/\t|\n|\s:/g, '')
+  .replace(/.*:/g, ' ')
+
+const get = async () => {
+  const $ = await scrape(URL)
+  console.log($.text)
+  const $row = $('table tbody tr')
+  $row.each((index, el) => {
+    const equipo = $(el).find('.fs-table-text_3').text().trim()
+    console.log(cleanText(equipo))
+  })
+}
+
+await get()
